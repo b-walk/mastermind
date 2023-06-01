d3843bb585c2816b21f467635cccbf7af32cd336
Blake <blameswalker@gmail.com>
2023-05-31 18:02:24 -0600

    Create Guess and Board classes to return guesses and key pegs, respectively

--- mastermind.rb
+++ mastermind.rb
@@ -1,3 +1,4 @@
+# Class Code generates a code sequence depending on an owner
 class Code
   def initialize(owner)
     @owner = owner
@@ -11,6 +12,34 @@ class Code
     when :computer
       puts "The computer has generated a code."
       Array.new(4).map { |_chr| %w[red green blue orange yellow pink].sample }
+    else
+      puts "error"
+    end
+  end
+end
+
+# Class Board returns a guess's key pegs
+class Board
+  attr_accessor :key_pegs
+
+  def initialize
+    @key_pegs = []
+  end
+end
+
+# Class Guess generates a guess depending on the guesser
+class Guess
+  def initialize(guesser)
+    @guesser = guesser
+  end
+
+  def get
+    case @guesser
+    when :human
+      puts "Enter your guess:"
+      gets.chomp.downcase.split(' ')
+    when :computer
+      # GUESSING ALGORITHM HERE
     else
       puts "error"
     end
