93ec94c1ad992beb96c281436374693c6b7de7dc
Blake <blameswalker@gmail.com>
2023-06-02 13:55:57 -0600

    Simplify Sequence and Peg objects for readability

--- mastermind.rb
+++ mastermind.rb
@@ -2,49 +2,35 @@
 class Peg
   attr_accessor :color, :status
 
-  def initialize(color)
-    @color = color
+  def initialize
+    @color = :none
     @status = :unused
   end
 end
 
 # Sequence objects generate code and guess sequences
 class Sequence
-  attr_accessor :owner, :type
+  attr_accessor :owner
 
   COLORS = %i[red blue green yellow orange pink]
   
-  def initialize(owner, type)
+  def initialize(owner)
     @owner = owner #human or computer
-    @type = type #guess or solution
-    @pegs = Array.new(4)
+    @pegs = Array.new(4, Peg.new)
   end
 
   def random #computer owner
-    puts "The computer has generated a code."
-    @pegs = @pegs.map { |_peg| Peg.new(COLORS.sample)}
+    @pegs = @pegs.map { |peg| peg.color = COLORS.sample}
   end
 
   def create #human owner
     @pegs = @pegs.map.with_index do |peg, position|
       puts "Enter peg no. #{position + 1}:"
-      Peg.new(gets.chomp.downcase.to_sym)
+      peg.color = gets.chomp.downcase.to_sym
     end
   end
 
   def pegs
     @pegs.each { |peg| print "#{peg.color} " }
-  end
-end
-
-computer_code = Sequence.new(:computer, :code)
-computer_code.random
-computer_code.pegs
-
-# Game objects track round counts and assign sequence owners
-class Game
-  attr_accessor :code, :maker, :breaker
-
-  def initialize
   end
 end
