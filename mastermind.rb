8f787f1190229e9354eeea0fd8a421a88a793ef6
Blake <blameswalker@gmail.com>
2023-06-05 17:21:58 -0600

    Create Code class which inherits basic traits from Sequence

--- mastermind.rb
+++ mastermind.rb
@@ -10,27 +10,35 @@ end
 
 # Sequence objects generate code and guess sequences
 class Sequence
-  attr_accessor :owner
+  attr_reader :owner
 
   COLORS = %i[red blue green yellow orange pink]
   
   def initialize(owner)
-    @owner = owner #human or computer
+    @owner = owner
     @pegs = Array.new(4, Peg.new)
-  end
-
-  def random #computer owner
-    @pegs = @pegs.map { |peg| peg.color = COLORS.sample}
-  end
-
-  def create #human owner
-    @pegs = @pegs.map.with_index do |peg, position|
-      puts "Enter peg no. #{position + 1}:"
-      peg.color = gets.chomp.downcase.to_sym
-    end
   end
 
   def pegs
     @pegs.each { |peg| print "#{peg.color} " }
   end
 end
+
+class Code < Sequence
+  attr_accessor :cracked
+
+  def computer
+    @pegs.map! { |peg| peg.color = COLORS.sample}
+  end
+
+  def human
+    puts "Four pegs are required. Valid colors include red, blue, green, yellow, orange, and pink."
+    @pegs.map! do |peg|
+      puts "Enter a color:"
+      peg.color = gets.chomp.downcase.to_sym
+    end
+  end
+end
+
+code = Code.new(:computer)
+code.pegs
